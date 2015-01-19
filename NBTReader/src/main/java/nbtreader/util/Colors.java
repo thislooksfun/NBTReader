897c5fc82e31720a798936e259a56aa9f8db558b
thislooksfun <thislooksfun@repbot.org>
2015-01-18 18:18:30 -0600

    Made the Colors class a bit cleaner

--- NBTReader/src/main/java/nbtreader/util/Colors.java
+++ NBTReader/src/main/java/nbtreader/util/Colors.java
@@ -1,8 +1,11 @@
 package nbtreader.util;
 
+import net.minecraft.util.MathHelper;
+
 /**
  * @author thislooksfun
  */
+@SuppressWarnings({"UnusedDeclaration", "SameParameterValue", "WeakerAccess"})
 public class Colors
 {
 	public static final int BLACK = rgb(0, 0, 0);
@@ -10,34 +13,31 @@ public class Colors
 	public static final int TEXT_COLOR = rgb(244, 244, 244);
 	public static final int DISABLED_TEXT_COLOR = rgb(112, 112, 112);
 	
-	public static int rgba(int r, int g, int b)
+	public static int argb(int r, int g, int b)
 	{
-		return rgba(r, g, b, 255);
+		return argb(255, r, g, b, 0);
 	}
-	public static int rgba(int r, int g, int b, int a)
+	public static int argb(int a, int r, int g, int b, int x)
 	{
-		a = (a > 255 ? 255 : a);
+		a = MathHelper.clamp_int(a, 0, 255);
 		return (a << 24) | rgb(r, g, b);
 	}
 	public static int rgb(int r, int g, int b)
 	{
-		r = (r > 255 ? 255 : r);
-		g = (g > 255 ? 255 : g);
-		b = (b > 255 ? 255 : b);
+		r = MathHelper.clamp_int(r, 0, 255);
+		g = MathHelper.clamp_int(g, 0, 255);
+		b = MathHelper.clamp_int(b, 0, 255);
 		return (r << 16) | (g << 8) | b;
 	}
 	
-	@SuppressWarnings("SameParameterValue")
 	public static int toRgba(int i)
 	{
 		return toRgba(i, 255);
 	}
-	@SuppressWarnings({"SameParameterValue", "WeakerAccess"})
 	public static int toRgba(int i, int alpha)
 	{
 		return (alpha << 24) | i;
 	}
-	@SuppressWarnings("UnusedDeclaration")
 	public static int toRgb(int i)
 	{
 		return i & ~(255 << 24);
